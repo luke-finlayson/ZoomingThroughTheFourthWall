487b2f4c3458a359dda12e73a1fc9b31d9031c07
Caleb Archer <calebandcorey@gmail.com>
2022-05-18 21:53:24 +1200

    Update port

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const https = require('https');
 const fs = require('fs');
-const port = 3000;
+const port = 8080;
 
 // Get the key and certificate require for HTTPS
 const credentials = {
