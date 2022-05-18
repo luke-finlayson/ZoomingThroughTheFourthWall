987edc784962aa007dc210553eeb8793e3147aa2
Caleb Archer <calebandcorey@gmail.com>
2022-05-18 20:43:40 +1200

    Add socketevents.js with new SocketEvents class

---
+++ socketevents.js
@@ -0,0 +1,10 @@
+// A pseudo-static class used for providing solid event names (prevent misspelling)
+class SocketEvents {
+    static Connection = "connection";
+    static Disconnect = "disconnect";
+    static NewMessage = "new message";
+
+    // Default static initialiser
+    static { }
+}
+
