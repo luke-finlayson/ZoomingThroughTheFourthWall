372d7cf20095c31aaa4fe33185ea168bff0a3ffe
Caleb Archer <calebandcorey@gmail.com>
2022-05-30 19:40:30 +1200

    Add textRecognition.js

---
+++ textRecognition.js
@@ -0,0 +1,35 @@
+const vision = require("@google-cloud/vision");
+const files = require("fs");
+const crypto = require('crypto');
+
+class TextRecognition{
+    constructor() {
+        this.client = new vision.ImageAnnotatorClient();
+    }
+
+    async getTextData(image64) {
+        fileName = "/image_cache/" + crypto.randomUUID();
+        files.writeFileSync(fileName, image64);
+
+        // Request for text recognition to be performed on the given image
+        const [result] = await this.client.textDetection(fileName);
+        const detections = result.textAnnotations;
+        let output = new Map();
+
+        if (!detections.length) {
+            console.log("No text detected");
+            return 
+        }
+
+        detections.forEach(text => {
+            output.set(text.description, text.boundingPoly.normalizedVertices);
+        });
+
+        // Get rid of the temporary image
+        files.rmSync(fileName);
+
+        return output;
+    }
+}
+
+module.exports = TextRecognition;
