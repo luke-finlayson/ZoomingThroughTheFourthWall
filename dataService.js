010a3b8b05db9df880b26ffc0fa583d304fed4d9
Caleb Archer <calebandcorey@gmail.com>
2022-06-16 11:45:04 +1200

    Update dataService.js to fully implement queries and insertions

--- dataService.js
+++ dataService.js
@@ -1,39 +1,113 @@
-const {Pool,Client} = require('pg')
+const { Pool, Client } = require('pg')
+
+class Message {
+    authorID;
+    content;
+    roomID;
+    timeSent;
+
+    constructor(authorID, roomID, content, timeSent = new Date(Date.now()).toISOString()) {
+        this.authorID = authorID;
+        this.content = content;
+        this.roomID = roomID;
+        this.timeSent = timeSent;
+    }
+}
+
+class User {
+    id;
+    name;
+
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
 
 class DataService{
     constructor() {
         this.pool = new Pool({
-            user: "postgres",
-            host: "localhost",
-            database: "zoom",
-            password: "",
+            user: process.env.POSTGRES_USER,
+            host: process.env.POSTGRES_HOST,
+            database: process.env.POSTGRES_DB,
+            password: process.env.POSTGRES_PASSWORD,
             port: 5432
         });
+
+        this.client = new Client();
     }
 
-    insertUsername() {
-        this.pool.query("INSERT INTO account(name)VALUES(name)", (err, res) => {
-            console.log(err, res);
+    insertRoom(roomID, callback) {
+        this.pool.query(`INSERT INTO room VALUES('${roomID}')`, (err) => {
+            if (err)
+                console.log('There was a rather problematic room insertion error.');
+            else
+                console.log(`Successful insertion of room with ID ${roomID}`);
+            
+            callback(err);
         });
     }
 
-    getMessages() {
-        this.pool.query("SELECT * FROM account, messages", (err, res) => {
-            console.log(err, res);
+    insertUser(user, callback) {
+        this.pool.query(`INSERT INTO users(id, name) VALUES('${user.id}', '${user.name}')`, (err) => {
+            if (err)
+                console.log('There was a rather problematic user insertion error.');
+            
+            callback(err);
         });
     }
 
-    insertMessage(author, message) {
-        this.pool.query("INSERT INTO messages(name,message)VALUES(name,message)", (err,res) => {
-            console.log(err, res);
+    insertUserIntoRoom(userID, roomID, callback) {
+        this.pool.query(`INSERT INTO roomUsers VALUES('${userID}', '${roomID}')`, (err) => {
+            console.log(err);
+
+            if (callback)
+                callback(err);
+        });
+    }
+
+    insertMessage(message) {
+        var authorID = this.client.escapeLiteral(message.authorID);
+        var roomID = this.client.escapeLiteral(message.roomID);
+        var content = this.client.escapeLiteral(message.content);
+
+        this.pool.query(`INSERT INTO messages(id, user_id, room_id, message, timeSent) VALUES(uuid_generate_v4(), ${authorID}, ${roomID}, ${content}, '${message.timeSent}')`, (err) => {
+            if (err)
+                console.log(err);
         })
     }
 
-    getUserNames() {
-        this.pool.query("SELECT name FROM account", (err,res) => {
-            console.log(err, res);
-        })
+    getMessages(roomID, callback) {
+        this.pool.query(`SELECT * FROM messages WHERE room_id::text = '${this.client.escapeLiteral(roomID)}'`, (err, result) => {
+            if (err)
+                console.log(err);
+            
+            var messages = result?.rows.map((_value, _index, row) => {
+                return new Message(row[1], row[2], row[3], row[4]);
+            });
+            
+            callback(err, messages);
+        });
+    }
+
+    getUsersInRoom(roomID, callback) {
+        this.pool.query(`SELECT u.id, u.name FROM users u JOIN roomUsers r ON r.user_id = u.id WHERE r.room_id::text = '${roomID}'`, (err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            var users = result.rows.map((_value, _index, row) => {
+                return new User(row[0], row[1]);
+            });
+
+            callback(err, users);
+        });
     }
 }
 
-module.exports = DataService;
+module.exports = {
+    DataService: DataService,
+    User: User,
+    Message: Message
+};
