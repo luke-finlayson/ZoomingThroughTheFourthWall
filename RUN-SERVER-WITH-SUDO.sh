aed1c5619981a7f449c3087a0c94b02fac58e78c
David Bainbridge <davidb@waikato.ac.nz>
2022-06-26 00:05:26 -0400

    Additional files that map out how to run the project on a VM run through VirtualBox

---
+++ RUN-SERVER-WITH-SUDO.sh
@@ -0,0 +1,3 @@
+
+sudo ./_RUN-SERVER.sh
+
---
+++ _RUN-SERVER.sh
@@ -0,0 +1,7 @@
+
+export POSTGRES_PASSWORD=admin
+export POSTGRES_USER=postgres
+export POSTGRES_DB=fourth_wall_cache
+export POSTGRES_HOST=localhost
+
+node ./server.js
