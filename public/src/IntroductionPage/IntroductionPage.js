9ae070ac23f6dd99679d844df24d91c3b6c33dbc
qwertyhpp <lukefin002@gmail.com>
2022-07-05 17:48:58 +1200

    Added socket connected state check

--- public/src/IntroductionPage/IntroductionPage.js
+++ public/src/IntroductionPage/IntroductionPage.js
@@ -1,16 +1,28 @@
 import './IntroductionPage.css'
 import ConnectButton from "./ConnectButton";
+import { useInterval } from "../RoomPage/useInterval.js";
+import { useState } from "react";
 
 const IntroductionPage = ({ socket }) => {
+
+  const [socketConnected, setSocketConnected] = useState(false);
+
+  useInterval(() => {
+    // Ensure socket is not null
+    if (socket) {
+      if (socket.connected) {
+        // If the socket connection has been made, update state
+        setSocketConnected(true);
+      }
+    }
+  }, 100);
 
   return (
     <div className='introduction_page_container'>
       <div className='introduction_page_panel'>
         {/* Change the logo here!! */}
         <h1>Fourth Wall</h1>
-        {socket &&
-          (socket.connected ? <ConnectButton /> : <div className="loader"></div>)
-        }
+        {socketConnected ? <ConnectButton /> : <div className="loader"></div>)}
       </div>
     </div>
   );
