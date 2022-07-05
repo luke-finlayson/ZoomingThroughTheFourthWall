df941ec2601adfa160b725d3d3840ecb8375c0e9
qwertyhpp <lukefin002@gmail.com>
2022-07-06 01:46:52 +1200

    Fixed issue with validating room names

--- public/src/JoinRoomPage/JoinRoomInputs.js
+++ public/src/JoinRoomPage/JoinRoomInputs.js
@@ -1,4 +1,6 @@
 import SocketEvents from '../RoomPage/socketevents';
+import { store } from '../store/store';
+import { useState } from 'react';
 
 const Input = ({ placeholder, value, changeHandler, handleKeyPress }) => {
     return (
@@ -17,22 +19,27 @@ const JoinRoomInputs = (props) => {
     const { nameValue, setNameValue,
       roomIdValue, setRoomIdValue, handleEnter, socket } = props;
 
+    const [isRoomHost, setIsRoomHost] = useState(false);
+
     // Handlers
     const handleRoomIdValueChange = (event) => {
         // Update global value
         setRoomIdValue(event.target.value);
 
         // Check room name
-        socket.emit(SocketEvents.CheckRoomId, roomIdValue, (response) => {
+        socket.emit(SocketEvents.CheckRoomId, event.target.value, (response) => {
           // If the room doesn't exist, notify user that it'll be created
           if (response === 'Does Not Exist') {
             // User is host
-            console.log("host");
+            setIsRoomHost(true);
           }
-          if (response === 'Exists') {
+          if (response === 'Exists' || response === 'Null') {
             // User won't be host
-            console.log("not host");
+            setIsRoomHost(false);
           }
+
+          store.dispatch({ type: 'SET_ROOM_HOST', payload: isRoomHost });
+          console.log(store.getState().isRoomHost);
         });
     }
 
@@ -42,6 +49,10 @@ const JoinRoomInputs = (props) => {
 
     return (
     <div className="join_room_inputs_container">
+        {isRoomHost &&
+          <p className="join-message">
+            Looks like that room doesn't exist, we'll create it for you
+          </p>}
         <Input
         placeholder="Enter meeting ID"
         value={roomIdValue}
