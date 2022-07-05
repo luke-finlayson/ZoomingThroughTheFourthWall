6505d07095a438883dc19793788b41fc34515636
qwertyhpp <lukefin002@gmail.com>
2022-07-05 15:28:08 +1200

    Added a loading indicator to prevent users trying to join rooms if the socket connection has not been made

--- public/src/IntroductionPage/IntroductionPage.js
+++ public/src/IntroductionPage/IntroductionPage.js
@@ -4,22 +4,23 @@ import ConnectingButtons from "./ConnectingButtons";
 import { connect } from 'react-redux'
 import { setIsRoomHost } from '../store/actions';
 
-const IntroductionPage = ({setIsRoomHostAction}) => {
-  
+const IntroductionPage = ({ setIsRoomHostAction, socket }) => {
+
   useEffect( () => {setIsRoomHostAction(false);
   }, []);
-  
+
   return (
     <div className='introduction_page_container'>
       <div className='introduction_page_panel'>
         {/* Change the logo here!! */}
-        <h1>Fourth Wall Project</h1>
-        <ConnectingButtons />
+        <h1>Fourth Wall</h1>
+        {socket &&
+          (socket.connected ? <ConnectingButtons /> : <div className="loader"></div>)
+        }
       </div>
     </div>
   );
 };
-
 const mapDispatchToProps = (dispatch) => {
   return {
     setIsRoomHostAction: (isRoomHost) => dispatch(setIsRoomHost(isRoomHost)),
@@ -27,4 +28,3 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 export default connect(null, mapDispatchToProps)(IntroductionPage);
-
