7cf3f1e49907cf56097205695a26ed3c16223b91
Caleb Archer <calebandcorey@gmail.com>
2022-07-05 20:22:25 +1200

    Remove dangling parenthesis

--- public/src/IntroductionPage/IntroductionPage.js
+++ public/src/IntroductionPage/IntroductionPage.js
@@ -22,7 +22,7 @@ const IntroductionPage = ({ socket }) => {
       <div className='introduction_page_panel'>
         {/* Change the logo here!! */}
         <h1>Fourth Wall</h1>
-        {socketConnected ? <ConnectButton /> : <div className="loader"></div>)}
+        {socketConnected ? <ConnectButton /> : <div className="loader"></div>}
       </div>
     </div>
   );
