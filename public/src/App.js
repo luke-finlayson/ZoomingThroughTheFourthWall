9d61f9380f1377d39ee3588a08d305208d35b3b5
lf108 <lf108@students.waikato.ac.nz>
2022-08-31 11:22:50 +1200

    Fixed bugs with socket connections

--- public/src/App.js
+++ public/src/App.js
@@ -5,28 +5,19 @@ import JoinPage from "./JoinPage/JoinPage";
 import './App.css';
 import io from 'socket.io-client';
 import { useInterval } from './Utilities/useInterval';
+import { store } from './store/store'
+
+
+// Attempt to create socket connection
+const _socket = io("//:443/");
 
 function App() {
-
-  const [socket, setSocket] = useState(null);
-
-  // Use polling to establish socket connection
-  useInterval(() => {
-    // Establish the socket connection if it hasn't already
-    if (socket === null || !socket.connected) {
-      // Attempt to create socket connection
-      const socket = io("//:443/");
-      // Store socket connection in state
-      setSocket(socket);
-    }
-  }, 100);
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<JoinPage socket={socket} />}>
+        <Route path="/" element={<JoinPage socket={_socket} />}>
         </Route>
-        <Route path='/room' element={<RoomPage socket={socket} />}>
+        <Route path='/room' element={<RoomPage socket={_socket} />}>
         </Route>
       </Routes>
     </Router>
--- public/src/RoomPage/RoomPage.js
+++ public/src/RoomPage/RoomPage.js
@@ -3,7 +3,6 @@ import './RoomPage.css';
 import VideoSection from './VideoSection/VideoSection';
 import ChatSection from './ChatSection/ChatSection';
 import { store } from '../store/store';
-import { useNavigate } from 'react-router-dom';
 
 // Array to keep track of all connected streams
 var streams = [];
@@ -11,14 +10,13 @@ var streams = [];
 // Entry point and interface of our room page
 const RoomPage = ({ socket }) => {
 
-  const navigate = useNavigate();
-
   useEffect(() => {
     // Get the state of the store
     const state = store.getState();
     // Return to home page if user has no username
-    if (state.username === null || state.username === "") {
-      navigate("/");
+    if (state.username === null || state.username === "unnamed") {
+      // Send the user back to the landing page
+      window.location.replace(window.location.protocol + "//" + window.location.host);
     }
   })
 
