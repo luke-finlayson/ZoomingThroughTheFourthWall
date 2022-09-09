2bf570ed9bf7137fef416d10e11e00d2e2e77d45
lf108 <lf108@students.waikato.ac.nz>
2022-09-09 12:14:06 +1200

    Added scrollable section for unfocused video frames

--- public/src/RoomPage/VideoSection/FocusView.js
+++ public/src/RoomPage/VideoSection/FocusView.js
@@ -1,4 +1,6 @@
 import VideoFrame from "./VideoFrame"
+import CollapseIcon from '../../resources/images/collapse.svg'
+import { useState } from "react"
 
 // Renders the streams with the screen shares displayed front and centre
 const FocusView = ({ 
@@ -7,6 +9,8 @@ const FocusView = ({
     setSelectedUser,
     updateStreamDimensions
 }) => {
+
+    const [collapsed, setCollapsed] = useState(false);
 
     return (
         <div className="video_focus_container">
@@ -22,6 +26,26 @@ const FocusView = ({
                     />
                 )
             })}
+            
+            <div className="video_small_carousel" style={collapsed ? {left: "-242px"} : {left: "10px"}}>
+                <div className="carousel_collapse_button" onClick={() => setCollapsed(!collapsed)}>
+                    <img 
+                    className={collapsed ? "flipped_X" : "not_flipped_X"}
+                    src={CollapseIcon} />
+                </div>
+                {streamsState.filter(user => !user.isDisplayMedia).map((screenShare, index) => {
+                    return (
+                        <VideoFrame
+                        key={index}
+                        user={screenShare}
+                        height={"auto"}
+                        selectedUser={selectedUser}
+                        setSelectedUser={setSelectedUser}
+                        updateStreamDimensions={updateStreamDimensions}
+                        />
+                    )
+                })}
+            </div>
         </div>
     )
 }
